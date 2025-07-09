8b2cc556c2faba173d85ae4311777d507ba781db
Sixels <sixels@protonmail.com>
2025-07-09 10:41:28 -0300

    Implementa Mochila (Augusto)

--- backpack.py
+++ backpack.py
@@ -2,23 +2,36 @@ from item import Item
 
 
 class Backpack:
-    itens: list[Item]
+    max_itens: int
+    items: list[Item]
 
-    def __init__(self):
-        self.itens = []
+    def __init__(self, max_itens: int = 10):
+        self.items = []
+        self.max_itens = max_itens
 
     def insert_item(self, item: Item) -> bool:
         """Insere um item no topo da mochila. Retorna True se a inserção for bem-sucedida, False caso contrário."""
-        pass
+        insertion_suceed: bool
+
+        if len(self.items) < self.max_itens:
+            self.items.append(item)
+            insertion_suceed = True
+        else:
+            insertion_suceed = False
+
+        return insertion_suceed
 
     def list_items(self) -> list[Item]:
         """Retorna uma lista dos itens contidos na mochila."""
-        return self.itens
+        return self.items
 
     def pick_item(self) -> Item | None:
         """Remove um item do topo da mochila. Retorna o item removido ou None se a mochila estiver vazia."""
-        pass
+        return self.items.pop()
 
     def get_item(self) -> Item | None:
         """Retorna o item do topo da mochila ou None se a mochila estiver vazia."""
-        pass
+        if len(self.items) == 0:
+            return None
+        else:
+            return self.items[-1]
