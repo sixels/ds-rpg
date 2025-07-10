419370f39e65c4580f8e3cb748ef70a1b3f6e2ef
Sixels <sixels@protonmail.com>
2025-07-10 10:48:29 -0300

    Adiciona um mapa padrão

--- main.py
+++ main.py
@@ -3,13 +3,45 @@ from room import Room
 
 
 def setup_rooms(entry_room: Room):
-    """Configura as salas e suas conexões."""
-    # Criando outras salas
-    room2 = Room("Sala 2")
-    room3 = Room("Sala 3")
+    """
+    Configura as salas e suas conexões.
+
+                         sala 6 -- sala 7
+                           |         |
+    entrada -- sala 4 -- sala 5 -- sala 8 --  sala 12 - sala 13
+      |         |          |                    |
+    sala 2 -- sala 3     sala 9 -- sala 10 -- sala 11
+    """
+
+    room2 = Room("Sala 2 (monstro)")
+    room3 = Room("Sala 3 (bau)")
+
+    room4 = Room("Sala 4 (monstro)")
+    room5 = Room("Sala 5 (vazia)")
+    room6 = Room("Sala 6 (monstro e bau)")
+    room7 = Room("Sala 7 (bau)")
+    room8 = Room("Sala 8 (vazia)")
+    room9 = Room("Sala 9 (bau)")
+    room10 = Room("Sala 10 (monstro)")
+    room11 = Room("Sala 11 (bau)")
+    room12 = Room("Sala 12 (monstro)")
+    room13 = Room("Sala 13 (monstro final)")
+
+    entry_room.set_exits(south=room2, east=room4)
+
+    room2.set_exits(east=room3)
+    room3.set_exits(north=room4)
+    room4.set_exits(east=room5)
+    room5.set_exits(north=room6, south=room9, east=room8)
+    room6.set_exits(east=room7)
+    room7.set_exits(south=room8)
+    room8.set_exits(east=room12)
+    room9.set_exits(east=room10)
+    room10.set_exits(east=room11)
+    room11.set_exits(north=room12)
+    room12.set_exits(east=room13)
 
     # Definindo as saídas
-    entry_room.set_exits(north=room2, east=room3)
 
 
 def main():
@@ -33,25 +65,35 @@ def main():
         if room.exits.west:
             typed_print("- Oeste")
 
-        command = input("sua ação > ").strip().lower()
+        while True:
+            command = input("sua ação > ").strip().lower()
 
-        if command == "norte" and room.exits.north:
-            room = room.exits.north
-        elif command == "sul" and room.exits.south:
-            room = room.exits.south
-        elif command == "leste" and room.exits.east:
-            room = room.exits.east
-        elif command == "oeste" and room.exits.west:
-            room = room.exits.west
-        else:
-            typed_print(
-                "Direção inválida ou não há saída nessa direção. Tente novamente."
-            )
-            continue
+            match command:
+                case "norte":
+                    if room.exits.north:
+                        room = room.exits.north
+                        break
+
+                case "sul":
+                    if room.exits.south:
+                        room = room.exits.south
+                        break
+                case "leste":
+                    if room.exits.east:
+                        room = room.exits.east
+                        break
+                case "oeste":
+                    if room.exits.west:
+                        room = room.exits.west
+                        break
+                case _:
+                    typed_print("Comando inválido. Tente novamente.")
+                    continue
+            typed_print("não há saídas nessa direção.")
 
 
 if __name__ == "__main__":
     try:
         main()
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, EOFError):
         pass
